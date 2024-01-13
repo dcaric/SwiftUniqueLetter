6cb413eb40ef5281d274b03110d3e5782fedbf2d
Dario Caric <dario.caric@gmail.com>
2024-01-13 20:42:54 +0100

    first commit

--- UniqueLetter/main.swift
+++ UniqueLetter/main.swift
@@ -7,5 +7,34 @@
 
 import Foundation
 
-print("Hello, World!")
+let str: String = "asjfhdsjfhjk"
+
+var dict: [Character : Int] = [:]
+
+var i = 0
+for item in str {
+    print("s:\(item)")
+    if (i == 0) {
+        dict[item] = 1
+    } else {
+        var foundLetter: Bool = false
+        for (key, elem) in dict {
+            if (item == key) {
+                dict[key] = elem + 1
+                foundLetter = true
+                break
+            }
+        }
+        if (!foundLetter) {
+            dict[item] = 1
+        }
+    }
+        
+        
+    i += 1
+}
+
+for (key, val) in dict {
+    print("key:\(key)  val:\(val)")
+}
 
